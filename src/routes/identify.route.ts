6fd13b739133b30a251c5e94884ee7dc3cd66e51
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-02-09 22:28:59 +0530

    feat: :sparkles: add identity validate middleware to identity route

--- src/routes/identify.route.ts
+++ src/routes/identify.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { identify } from "../controllers/identify.controller";
+import { identifyValidate } from "../middlewares/identify.validate";
 const router = express.Router();
 
-router.post("/identify", identify);
+router.post("/identify", identifyValidate, identify);
 
 export default router;
